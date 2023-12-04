cd07447ebcaa2623356ee6e91abfb233b29cdcaa
joaquin <joaquinagras@gmail.com>
2023-12-04 15:31:01 +0100

    with csrf token not good

--- php_send_data.php
+++ php_send_data.php
@@ -1,15 +1,21 @@
-<?php
+ <?php
 session_set_cookie_params(['lifetime'=>0,'path'=>'/','domain'=>$_SERVER['HTTP_HOST'],'secure'=>false,'httponly'=>true,'samesite'=>'Strict']);
 session_start();
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
-// Check if the logout button is clicked
+// Check if the logout button is clicked and verify the CSRF token
 if (isset($_POST["logout"])) {
-    setcookie("user","",time()-3600,"/");
-    if (isset($_COOKIE[session_name()])) { 
-        setcookie(session_name(),"",time()-3600,"/");
+    // Check if the CSRF token is valid
+    if (!isset($_POST["csrf_token"]) || $_POST["csrf_token"] !== $_SESSION['csrf_token']) {
+        echo "CSRF detected, the action will be aborted";
+        exit();
+    }
+
+    setcookie("user", "", time() - 3600, "/");
+    if (isset($_COOKIE[session_name()])) {
+        setcookie(session_name(), "", time() - 3600, "/");
     }
     session_destroy();
     header("Location: /");
@@ -24,53 +30,59 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
     $password = $_POST["password"];
 
     try {
-        $conn = mysqli_connect("localhost","root","hacker2016","test_app");
-        if (!$conn) { 
-            die("Error en la conexión a la BBDD: ".mysqli_connect_error());
+        $conn = mysqli_connect("localhost", "root", "hacker2016", "test_app");
+        if (!$conn) {
+            die("Error en la conexión a la BBDD: " . mysqli_connect_error());
         } else {
             $stmt = $conn->prepare("SELECT * FROM usuarios WHERE username=? AND password=?");
-            $stmt->bind_param("ss",$username,$password);
+            $stmt->bind_param("ss", $username, $password);
             $stmt->execute();
             $result = $stmt->get_result();
 
-            if ($result === false) { 
-                die("Error en la consulta a la BBDD: ".mysqli_error($conn));
+            if ($result === false) {
+                die("Error en la consulta a la BBDD: " . mysqli_error($conn));
             } elseif ($result->num_rows > 0) {
                 $unique_string = bin2hex(random_bytes(16));
-                $cookie_options = ['expires'=>time()+(86400*30),'path'=>'/','secure'=>false,'httponly'=>true,'samesite'=>'Strict'];
-                setcookie('user',$unique_string,$cookie_options);
-                if (isset($_COOKIE["user"])) { 
+                $cookie_options = ['expires' => time() + (86400 * 30), 'path' => '/', 'secure' => false, 'httponly' => true, 'samesite' => 'Strict'];
+                setcookie('user', $unique_string, $cookie_options);
+                if (isset($_COOKIE["user"])) {
                     $username = $_COOKIE["user"];
                 }
-	    // username sanitized	
             } else {
-		$username_sanitized = htmlspecialchars($username, ENT_QUOTES, 'UTF-8');    
+                $username_sanitized = htmlspecialchars($username, ENT_QUOTES, 'UTF-8');
                 echo "<p>El usuario $username_sanitized o su contraseña no existe.</p>";
                 exit();
             }
         }
-    } catch (Exception $e) { 
+    } catch (Exception $e) {
         echo 'Caught exception: ', $e->getMessage(), "\n";
     }
 }
 
+// Generate and store the CSRF token after successful login
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+    $csrf_token = bin2hex(random_bytes(16));
+    $_SESSION['csrf_token'] = $csrf_token;
+}
+
 // Display welcome message and user information if the username exists
 if ($username) {
-    // Sanitize the username before outputting it
     $username_sanitized = htmlspecialchars($username, ENT_QUOTES, 'UTF-8');
-
     echo "<h1>Bienvenido a la aplicación</h1>";
-    // Use the sanitized username in the output
     echo "<p>El usuario $username_sanitized existe, ¡Bienvenido!</p>";
-    echo '<img src="/'.$username_sanitized.'/'.$username_sanitized.'.jpg" alt="Imagen de bienvenida" width="200" height="150">';
-    
-    // Display the .txt files
+    echo '<img src="/' . $username_sanitized . '/' . $username_sanitized . '.jpg" alt="Imagen de bienvenida" width="200" height="150">';
+
     $files = glob("/var/www/html/VULNERABLE/$username_sanitized/*.txt");
-    foreach($files as $file) { 
-        echo "<p><a href='/".$username_sanitized."/".basename($file)."'>".basename($file)."</a></p>";
+    foreach ($files as $file) {
+        echo "<p><a href='/" . $username_sanitized . "/" . basename($file) . "'>" . basename($file) . "</a></p>";
     }
 
-    // Display the logout button
-    echo '<form method="post"><input type="hidden" name="logout" value="true"><input type="submit" value="Cerrar sesión y borrar cookie"></form>';
+    // Display the logout button with the CSRF token
+    echo '<form method="post">';
+    echo '<input type="hidden" name="csrf_token" value="' . $_SESSION['csrf_token'] . '">';
+    echo '<input type="hidden" name="logout" value="true">';
+    echo '<input type="submit" value="Cerrar sesión y borrar cookie">';
+    echo '</form>';
 }
 ?>
+
